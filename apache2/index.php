6f4199ac47525e443660c81c621e70eb2b2d335f
Abhishek Jha <57244049+abhishek0795@users.noreply.github.com>
2021-12-13 12:28:09 +0530

    Update index.php

--- apache2/index.php
+++ apache2/index.php
@@ -1,9 +1,13 @@
 <?php
 #$servername = getenv("MYSQL_HOST");
-$servername ="myDB";
-$username = getenv("MYSQL_USER");
-$password = getenv("MYSQL_PASSWORD");
-$dbname = getenv("MYSQL_DATABASE");
+// $servername ="myDB";
+// $username = getenv("MYSQL_USER");
+// $password = getenv("MYSQL_PASSWORD");
+// $dbname = getenv("MYSQL_DATABASE");
+$servername ="abhishek-rds.cq174vfsoqja.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "admin123";
+$dbname = "testdb";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -11,18 +15,20 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-  }
-} else {
-  echo "0 results";
+else{
+  echo "connected successfully";
 }
+// $sql = "SELECT id, firstname, lastname FROM MyGuests";
+// $result = mysqli_query($conn, $sql);
 
-mysqli_close($conn);
+// if (mysqli_num_rows($result) > 0) {
+//   // output data of each row
+//   while($row = mysqli_fetch_assoc($result)) {
+//     echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+//   }
+// } else {
+//   echo "0 results";
+// }
+
+// mysqli_close($conn);
 ?>
